98db4b0cb1d38c0f91fa20cef59996dea5ca4ff2
jwheller0013 <jwheller0013@yahoo.com>
2025-04-21 16:00:07 -0400

    trying

---
+++ My_Collection/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from My_Collection.routes import rt
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    app.register_blueprint(rt)
+    from My_Collection.models import db
+    db.init_app(app)
+
+
+    with app.app_context():
+        db.create_all()
+        return app
