3e46f4bfc08c81cae76de5ee3d66bb52f9d9ded9
jwheller0013 <jwheller0013@yahoo.com>
2025-04-23 15:08:01 -0400

    test entries made

--- My_Collection/test_entries.py
+++ My_Collection/test_entries.py
@@ -5,9 +5,29 @@ app = create_app()
 
 def add_entries():
     entries_to_add = [
-        {"id": 2, "user_id": 1, "collection_id": 1, "title": "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-         "tv_film": 1, "rating": 8.118, "link": "https://www.imdb.com/title/tt0057012", "poster": "https://image.tmdb.org/t/p/original/6x7MzQ6BOMlRzam1StcmPO9v61g.jpg",
-         "upc": "43396061873", "genres": [35, 10752]}
+        # {"id": 2, "user_id": 1, "collection_id": 1, "title": "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+        #  "tv_film": 1, "rating": 8.118, "link": "https://www.imdb.com/title/tt0057012", "poster": "https://image.tmdb.org/t/p/original/6x7MzQ6BOMlRzam1StcmPO9v61g.jpg",
+        #  "upc": "43396061873", "genres": [35, 10752]},
+        {"id": 3, "user_id": 1, "collection_id": 1,
+         "title": "Seven Samurai",
+         "tv_film": 1, "rating": 8.457, "link": "https://www.imdb.com/title/tt0047478",
+         "poster": "https://image.tmdb.org/t/p/original/8OKmBV5BUFzmozIC3pPWKHy17kx.jpg",
+         "upc": "715515019927", "genres": [28, 18]},
+        {"id": 4, "user_id": 1, "collection_id": 1,
+         "title": "The Usual Suspects",
+         "tv_film": 1, "rating": 8.176, "link": "https://www.imdb.com/title/tt0114814",
+         "poster": "https://image.tmdb.org/t/p/original/rWbsxdwF9qQzpTPCLmDfVnVqTK1.jpg",
+         "upc": "27616780126", "genres": [18, 80, 53]},
+        {"id": 5, "user_id": 1, "collection_id": 1,
+         "title": "Thank you for Smoking",
+         "tv_film": 1, "rating": 7.193, "link": "https://www.imdb.com/title/tt0427944",
+         "poster": "https://image.tmdb.org/t/p/original/cJpeM7U36diFinieBWNLVi0FlQz.jpg",
+         "upc": "24543255048", "genres": [35, 18]},
+        {"id": 6, "user_id": 1, "collection_id": 1,
+         "title": "Oldboy",
+         "tv_film": 1, "rating": 8.25, "link": "https://www.imdb.com/title/tt0364569",
+         "poster": "https://image.tmdb.org/t/p/original/pWDtjs568ZfOTMbURQBYuT4Qxka.jpg",
+         "upc": "842498030042", "genres": [18, 53, 9648, 28]},
         ]
 
     for entry_data in entries_to_add:
